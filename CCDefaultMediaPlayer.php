ac3b370e6bd59e66bb95837fb2799c3d25630d8a
ChrisRidings <chris@chrisridings.com>
2017-03-12 20:58:34 +0100

    Inheritance in CCDefaultMediaPlayer - CCBaseSender
    Seperated inheritable functions/properties from CCDefaultMediaPlayer into CCBaseSender, making it easier to add future functionality for different receivers.

--- CCDefaultMediaPlayer.php
+++ CCDefaultMediaPlayer.php
@@ -2,41 +2,10 @@
 
 // Make it really easy to play videos by providing functions for the Chromecast Default Media Player
 
-class CCDefaultMediaPlayer
-{
-	
-	public $chromecast; // The chromecast the initiated this instance.
-	public $mediaid; // The media session id.
-	
-	public function __construct($hostchromecast) {
-		$this->chromecast = $hostchromecast;
-	}
+require_once("CCBaseSender.php");
 
-	public function launch() {
-		// Launch the player or connect to an existing instance if one is already running
-		// First connect to the chromecast
-		$this->chromecast->transportid = "";
-		$this->chromecast->cc_connect();
-		$s = $this->chromecast->getStatus();
-		// Grab the appid
-		preg_match("/\"appId\":\"([^\"]*)/",$s,$m);
-		$appid = $m[1];
-		if ($appid == "CC1AD845") {
-			// Default Media Receiver is live
-			$this->chromecast->getStatus();
-			$this->chromecast->connect();
-		} else {
-			// Default Media Receiver is not currently live, start it.
-			$this->chromecast->launch("CC1AD845");
-			$this->chromecast->transportid = "";
-			$r = "";
-			while (!preg_match("/Ready to Cast/",$r) && !preg_match("/Default Media Receiver/",$r)) {
-				$r = $this->chromecast->getStatus();
-				sleep(1);
-			}
-			$this->chromecast->connect();
-		}
-	}
+class CCDefaultMediaPlayer extends CCBaseSender
+{
 	
 	public function play($url,$streamType,$contentType,$autoPlay,$currentTime) {
 		// Start a playing
